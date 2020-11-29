39c5a0dc4cb01eedfc68b888a9927d8df8edb0f7
Ognjen Radisic <radisicognjen@gmail.com>
2020-11-30 00:24:24 +0100

    sidebar and submenu

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,25 +6,36 @@ import { useGlobalContext } from './context'
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext()
 
+  const displaySubmenu = (e) => {
+    e.preventDefault()
+    openSubmenu()
+  }
+
   return (
     <nav className='nav'>
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} alt='stripe' />
-          <button className='btn toggle-btn' onClick={openSidebar}>
+          <button className='btn toggle-btn' onClick={displaySubmenu}>
             <FaBars />
           </button>
         </div>
 
         <ul className='nav-links'>
           <li>
-            <button className='link-btn'>products</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              products
+            </button>
           </li>
           <li>
-            <button className='link-btn'>developers</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              developers
+            </button>
           </li>
           <li>
-            <button className='link-btn'>company</button>
+            <button className='link-btn' onMouseOver={openSubmenu}>
+              company
+            </button>
           </li>
         </ul>
 
